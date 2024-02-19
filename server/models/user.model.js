1c4609e3ba02afd58370eab2b6c377652f0cd860
jpablo9988 <65927907+jpablo9988@users.noreply.github.com>
2024-02-19 12:19:35 -0500

    Added model & database connection

---
+++ server/models/user.model.js
@@ -0,0 +1,23 @@
+import { Double, Int32 } from 'bson'
+import mongoose from 'mongoose'
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String, 
+        trim: true,
+        required: [true,'a Name is required']
+        },
+    description: {
+        type: String, 
+        required: false
+        },
+    price: Double,
+    quantity: Int32,
+    category:
+    {
+        type: String,
+        required: [true, 'A category is requiered']
+    }
+
+  }) 
+export default mongoose.model('User', UserSchema)
